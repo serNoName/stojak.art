412b6fde923459e49eb9e40624babd8907b07535
serNoName <user15b12n5lkuidso2@gmail.com>
2024-05-04 13:18:05 +0200

    Organized by js

--- app/js/main.js
+++ app/js/main.js
@@ -80,31 +80,16 @@ $(document).ready(function () {
 	});
 });
 
-console.clear();
-
 class Grain {
 	constructor(el) {
-		/**
-		 * Options
-		 * Increase the pattern size if visible pattern
-		 */
 		this.patternSize = 150;
 		this.patternScaleX = 1;
 		this.patternScaleY = 1;
-		this.patternRefreshInterval = 3; // 8
-		this.patternAlpha = 15; // int between 0 and 255,
-
-		/**
-		 * Create canvas
-		 */
+		this.patternRefreshInterval = 3;
+		this.patternAlpha = 15;
 		this.canvas = el;
 		this.ctx = this.canvas.getContext("2d");
 		this.ctx.scale(this.patternScaleX, this.patternScaleY);
-
-		/**
-		 * Create a canvas that will be used to generate grain and used as a
-		 * pattern on the main canvas.
-		 */
 		this.patternCanvas = document.createElement("canvas");
 		this.patternCanvas.width = this.patternSize;
 		this.patternCanvas.height = this.patternSize;
@@ -113,27 +98,18 @@ class Grain {
 			this.patternSize,
 			this.patternSize,
 		);
-		this.patternPixelDataLength = this.patternSize * this.patternSize * 4; // rgba = 4
-
-		/**
-		 * Prebind prototype function, so later its easier to user
-		 */
+		this.patternPixelDataLength = this.patternSize * this.patternSize * 4;
 		this.resize = this.resize.bind(this);
 		this.loop = this.loop.bind(this);
-
 		this.frame = 0;
-
 		window.addEventListener("resize", this.resize);
 		this.resize();
-
 		window.requestAnimationFrame(this.loop);
 	}
-
 	resize() {
 		this.canvas.width = window.innerWidth * devicePixelRatio;
 		this.canvas.height = window.innerHeight * devicePixelRatio;
 	}
-
 	update() {
 		const {
 			patternPixelDataLength,
@@ -141,49 +117,31 @@ class Grain {
 			patternAlpha,
 			patternCtx,
 		} = this;
-
-		// put a random shade of gray into every pixel of the pattern
 		for (let i = 0; i < patternPixelDataLength; i += 4) {
-			// const value = (Math.random() * 255) | 0;
 			const value = Math.random() * 255;
-
 			patternData.data[i] = value;
 			patternData.data[i + 1] = value;
 			patternData.data[i + 2] = value;
 			patternData.data[i + 3] = patternAlpha;
 		}
-
 		patternCtx.putImageData(patternData, 0, 0);
 	}
-
 	draw() {
 		const { ctx, patternCanvas, canvas, viewHeight } = this;
 		const { width, height } = canvas;
-
-		// clear canvas
 		ctx.clearRect(0, 0, width, height);
-
-		// fill the canvas using the pattern
 		ctx.fillStyle = ctx.createPattern(patternCanvas, "repeat");
 		ctx.fillRect(0, 0, width, height);
 	}
-
 	loop() {
-		// only update grain every n frames
 		const shouldDraw = ++this.frame % this.patternRefreshInterval === 0;
 		if (shouldDraw) {
 			this.update();
 			this.draw();
 		}
-
 		window.requestAnimationFrame(this.loop);
 	}
 }
-
-/**
- * Initiate Grain
- */
-const el = document.getElementById("grain");
-const grain = new Grain(el);
+const grain = new Grain(document.getElementById("grain"));
 
 ScrollReveal().reveal(".picture");
