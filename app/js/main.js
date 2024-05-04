f4ee62be2eae8f3d7de0e29afb49d43009b9ee68
serNoName <user15b12n5lkuidso2@gmail.com>
2024-05-04 13:04:27 +0200

    First commit

---
+++ app/js/main.js
@@ -0,0 +1,189 @@
+"use strict";
+
+$(document).ready(function () {
+	$("#burger").on("click", function () {
+		const burgerBtn = $(this);
+		const menu = burgerBtn.closest(".menu");
+		const topMenu = menu.find(".menu__top");
+		const menuWrapper = $(".menu__wrapper");
+		menu.toggleClass("--menu-open");
+		burgerBtn.toggleClass("--burger-active");
+		topMenu.stop().toggle(500);
+		menuWrapper.attr("inert", "");
+
+		$(document).on("mouseleave", ".menu", function () {
+			$(this).removeClass("--menu-open");
+			topMenu.hide(300);
+			burgerBtn.removeClass("--burger-active");
+			menuWrapper.removeAttr("inert");
+		});
+	});
+	$(".menu__button").keydown(function (e) {
+		if (e.keyCode == 39) {
+			// right
+			e.preventDefault();
+			$(this)
+				.parent()
+				.next(".menu__li")
+				.children(".menu__button")
+				.focus();
+		} else if (e.keyCode == 37) {
+			//left
+			e.preventDefault();
+			$(this)
+				.parent()
+				.prev(".menu__li")
+				.children(".menu__button")
+				.focus();
+		}
+	});
+	$(".menu__link").keydown(function (e) {
+		const $parentList = $(this).parent();
+
+		if (e.keyCode == 38) {
+			// up
+			e.preventDefault();
+			$parentList
+				.prevAll(".menu__li")
+				.first()
+				.children(".menu__link")
+				.focus();
+		} else if (e.keyCode == 39) {
+			// right
+			e.preventDefault();
+			$parentList
+				.parent()
+				.next(".menu__list")
+				.children(".menu__li")
+				.first()
+				.children(".menu__link")
+				.focus();
+		} else if (e.keyCode == 37) {
+			//left
+			e.preventDefault();
+			$parentList
+				.parent()
+				.prev(".menu__list")
+				.children(".menu__li")
+				.first()
+				.children(".menu__link")
+				.focus();
+		} else if (e.keyCode == 40) {
+			// bottom
+			e.preventDefault();
+			$parentList
+				.nextAll(".menu__li")
+				.first()
+				.children(".menu__link")
+				.focus();
+		}
+	});
+});
+
+console.clear();
+
+class Grain {
+	constructor(el) {
+		/**
+		 * Options
+		 * Increase the pattern size if visible pattern
+		 */
+		this.patternSize = 150;
+		this.patternScaleX = 1;
+		this.patternScaleY = 1;
+		this.patternRefreshInterval = 3; // 8
+		this.patternAlpha = 15; // int between 0 and 255,
+
+		/**
+		 * Create canvas
+		 */
+		this.canvas = el;
+		this.ctx = this.canvas.getContext("2d");
+		this.ctx.scale(this.patternScaleX, this.patternScaleY);
+
+		/**
+		 * Create a canvas that will be used to generate grain and used as a
+		 * pattern on the main canvas.
+		 */
+		this.patternCanvas = document.createElement("canvas");
+		this.patternCanvas.width = this.patternSize;
+		this.patternCanvas.height = this.patternSize;
+		this.patternCtx = this.patternCanvas.getContext("2d");
+		this.patternData = this.patternCtx.createImageData(
+			this.patternSize,
+			this.patternSize,
+		);
+		this.patternPixelDataLength = this.patternSize * this.patternSize * 4; // rgba = 4
+
+		/**
+		 * Prebind prototype function, so later its easier to user
+		 */
+		this.resize = this.resize.bind(this);
+		this.loop = this.loop.bind(this);
+
+		this.frame = 0;
+
+		window.addEventListener("resize", this.resize);
+		this.resize();
+
+		window.requestAnimationFrame(this.loop);
+	}
+
+	resize() {
+		this.canvas.width = window.innerWidth * devicePixelRatio;
+		this.canvas.height = window.innerHeight * devicePixelRatio;
+	}
+
+	update() {
+		const {
+			patternPixelDataLength,
+			patternData,
+			patternAlpha,
+			patternCtx,
+		} = this;
+
+		// put a random shade of gray into every pixel of the pattern
+		for (let i = 0; i < patternPixelDataLength; i += 4) {
+			// const value = (Math.random() * 255) | 0;
+			const value = Math.random() * 255;
+
+			patternData.data[i] = value;
+			patternData.data[i + 1] = value;
+			patternData.data[i + 2] = value;
+			patternData.data[i + 3] = patternAlpha;
+		}
+
+		patternCtx.putImageData(patternData, 0, 0);
+	}
+
+	draw() {
+		const { ctx, patternCanvas, canvas, viewHeight } = this;
+		const { width, height } = canvas;
+
+		// clear canvas
+		ctx.clearRect(0, 0, width, height);
+
+		// fill the canvas using the pattern
+		ctx.fillStyle = ctx.createPattern(patternCanvas, "repeat");
+		ctx.fillRect(0, 0, width, height);
+	}
+
+	loop() {
+		// only update grain every n frames
+		const shouldDraw = ++this.frame % this.patternRefreshInterval === 0;
+		if (shouldDraw) {
+			this.update();
+			this.draw();
+		}
+
+		window.requestAnimationFrame(this.loop);
+	}
+}
+
+/**
+ * Initiate Grain
+ */
+const el = document.getElementById("grain");
+const grain = new Grain(el);
+
+ScrollReveal().reveal(".picture");
